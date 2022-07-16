114552de25dd13afdf22e4b1c0b8e89105297578
xenos#1337 <66328734+xenos1337@users.noreply.github.com>
2022-07-16 23:48:58 +0200

    draw strength

--- 3D Cursor Animation/Form1.Designer.cs
+++ 3D Cursor Animation/Form1.Designer.cs
@@ -32,10 +32,16 @@
             this.curX = new System.Windows.Forms.Label();
             this.curY = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.removeStrengthBtn = new System.Windows.Forms.Button();
+            this.addStrengthBtn = new System.Windows.Forms.Button();
+            this.str = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
@@ -45,6 +51,7 @@
             // 
             // curX
             // 
+            this.curX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.curX.AutoSize = true;
             this.curX.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.curX.Location = new System.Drawing.Point(12, 530);
@@ -55,6 +62,7 @@
             // 
             // curY
             // 
+            this.curY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.curY.AutoSize = true;
             this.curY.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.curY.Location = new System.Drawing.Point(12, 550);
@@ -65,6 +73,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(395, 530);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 57);
@@ -73,28 +82,72 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // removeStrengthBtn
+            // 
+            this.removeStrengthBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeStrengthBtn.Font = new System.Drawing.Font("Arial", 15F);
+            this.removeStrengthBtn.Location = new System.Drawing.Point(299, 560);
+            this.removeStrengthBtn.Name = "removeStrengthBtn";
+            this.removeStrengthBtn.Size = new System.Drawing.Size(90, 27);
+            this.removeStrengthBtn.TabIndex = 4;
+            this.removeStrengthBtn.Text = "-";
+            this.removeStrengthBtn.UseVisualStyleBackColor = true;
+            this.removeStrengthBtn.Click += new System.EventHandler(this.removeStrengthBtn_Click);
+            // 
+            // addStrengthBtn
+            // 
+            this.addStrengthBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addStrengthBtn.Font = new System.Drawing.Font("Arial", 15F);
+            this.addStrengthBtn.Location = new System.Drawing.Point(299, 530);
+            this.addStrengthBtn.Name = "addStrengthBtn";
+            this.addStrengthBtn.Size = new System.Drawing.Size(90, 27);
+            this.addStrengthBtn.TabIndex = 5;
+            this.addStrengthBtn.Text = "+";
+            this.addStrengthBtn.UseVisualStyleBackColor = true;
+            this.addStrengthBtn.Click += new System.EventHandler(this.addStrengthBtn_Click);
+            this.addStrengthBtn.MouseDown += AddStrengthBtn_MouseDown;
+            this.addStrengthBtn.MouseUp += AddStrengthBtn_MouseUp;
+            // 
+            // str
+            // 
+            this.str.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.str.AutoSize = true;
+            this.str.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.str.Location = new System.Drawing.Point(11, 571);
+            this.str.Name = "str";
+            this.str.Size = new System.Drawing.Size(67, 19);
+            this.str.TabIndex = 6;
+            this.str.Text = "Strength:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(516, 599);
+            this.Controls.Add(this.str);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.curY);
             this.Controls.Add(this.curX);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.removeStrengthBtn);
+            this.Controls.Add(this.addStrengthBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
         #endregion
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label curX;
         private System.Windows.Forms.Label curY;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button removeStrengthBtn;
+        private System.Windows.Forms.Button addStrengthBtn;
+        private System.Windows.Forms.Label str;
     }
 }
 
--- 3D Cursor Animation/Form1.cs
+++ 3D Cursor Animation/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,16 @@ namespace _3D_Cursor_Animation
     {
         private static Graphics formGraphics;
         private static Color col = Color.Red;
+        private static int strength;
+        private static Stopwatch sw;
 
         public Form1()
         {
             InitializeComponent();
 
             formGraphics = this.panel1.CreateGraphics();
+            strength = 3;
+            sw = new Stopwatch();
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
@@ -31,23 +36,63 @@ namespace _3D_Cursor_Animation
 
             if (e.Button == MouseButtons.Left)
             {
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - 1, point.Y, 3, 3));
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + 1 , point.Y, 3, 3));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y - strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y + strength, strength, strength));
 
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y - 1, 3, 3));
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y + 1, 3, 3));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - strength, point.Y, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + strength , point.Y, strength, strength));
 
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - 1, point.Y + 1, 3, 3));
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + 1, point.Y - 1, 3, 3));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y - strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X, point.Y + strength, strength, strength));
 
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + 1, point.Y + 1, 3, 3));
-                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - 1, point.Y - 1, 3, 3));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - strength, point.Y + strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + strength, point.Y - strength, strength, strength));
+
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + strength, point.Y + strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - strength, point.Y - strength, strength, strength));
+
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + strength, point.Y - strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X + strength, point.Y - strength, strength, strength));
+
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - strength, point.Y + strength, strength, strength));
+                formGraphics.FillRectangle(new SolidBrush(col), new Rectangle(point.X - strength, point.Y + strength, strength, strength));
+
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) => formGraphics.Clear(Color.Gainsboro);
+
+        private void Form1_ResizeEnd(object sender, EventArgs e) => formGraphics = this.panel1.CreateGraphics();
+
+        private void Form1_DoubleClick(object sender, EventArgs e) => formGraphics = this.panel1.CreateGraphics();
+
+        private void addStrengthBtn_Click(object sender, EventArgs e)
         {
-            formGraphics.Clear(Color.Gainsboro);
+            strength++;
+            str.Text = "Strength: " + strength;
+        }
+
+        private void removeStrengthBtn_Click(object sender, EventArgs e)
+        {
+            strength--;
+            str.Text = "Strength: " + strength;
+        }
+
+        private void AddStrengthBtn_MouseDown(object sender, EventArgs e) 
+        {
+            sw.Start();
+        }
+
+
+        private void AddStrengthBtn_MouseUp(object sender, EventArgs e)
+        {
+            sw.Stop();
+            if (sw.ElapsedMilliseconds <= 700 && sw.ElapsedMilliseconds >= 200)
+            {
+                strength += 10;
+                str.Text = "Strength: " + strength;
+            }
+            sw.Reset();
         }
     }
 }
